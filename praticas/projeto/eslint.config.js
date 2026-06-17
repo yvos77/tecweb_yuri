eb9f42af4522f9ddc2efec7077ff4b7057ae9a2a
f3lipe31 <lipecunha2005@gmail.com>
2026-06-16 22:19:24 -0300

    mudança chore: inicializa projeto Vite + React e dependencias (#01)

--- praticas/projeto/eslint.config.js
+++ praticas/projeto/eslint.config.js
@@ -24,10 +24,7 @@ export default [
       ...js.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
     },
   },
 ]
